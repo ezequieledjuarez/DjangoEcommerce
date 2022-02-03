504ac5b3ad4d2acc35400292f4016e53c021e806
EzeJuarez <ezequieledjuarez@gmail.com>
2022-02-03 06:34:28 -0300

    End logic mail in contact view

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,7 +1,12 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
+
 from django.views import generic
-from .forms import ContactForm
+from django.contrib import messages
 from django.urls import reverse
+from .forms import ContactForm
+
+from django.conf import settings
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
@@ -12,3 +17,28 @@ class ContactView(generic.FormView):
 
     def get_success_url(self) -> str:
         return reverse("contact")
+
+    def form_valid(self,form):
+        messages.info(
+            self.request,"Recibimos tu mensaje")
+        
+        name = form.cleaned_data.get('name')
+        email = form.cleaned_data.get('email')
+        message = form.cleaned_data.get('message')
+
+        full_message = f"""
+            Mensaje recibido de {name}, {email}
+            ____________________________________
+
+            {message}
+        """
+        send_mail(
+            subject="Mensaje recibido",
+            message=full_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.NOTIFY_EMAIL]
+        )
+           
+        
+            
+        return super(ContactView, self).form_valid(form)
